9ae6c8b83617068e5013aaf1565e7ff1fe9ddc31
Dominik Malek <dominik.malek@techmo.pl>
2022-01-07 22:25:59 +0100

    update /data

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require("express")
 var app = express()
 var PORT = process.env.PORT || 3000;
 
+const examp = { imie: "Dominik", nazwisko: "Małek", klasa: "3i1", grupa: "2" }
+
 app.get("/", function (req, res) {
     res.send("<h1>Małek Domink 3i1 Gr2</h1>")
+})
+
+app.get("/data", function (req, res) {
+    res.send(examp)
 })
 
 app.listen(PORT, function () {
